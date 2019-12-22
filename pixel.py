c25f3946dc7f4e615421e16054e5e3d942a6a317
mazunki <rolferen@gmail.com>
2019-12-22 21:59:37 +0100

    created classes

--- pixel.py
+++ pixel.py
@@ -2,20 +2,89 @@ import time
 import board
 import neopixel
 
+COLORS = {
+    "BLACK" = (0,0,0),
+    "WHITE" = (255,255,255)
+}
+GPIO_PIN = board.D18
+COLOR_SETTING = neopixel.GRB
 
-pixel_pin = board.D18
+class Field:
+    def __init__(self, boards, size_board_x=8, size_board_y=8):
+        self.boards = []
+        self.neofield = neopixel.NeoPixel(
+                            GPIO_PIN,
+                            boards*size_board_x*size_board_y,
+                            pixel_order=COLOR_SETTING
+                        )
+        for board_n in range(boards_y):
+            new_board = Board(
+                            size_board_x,
+                            size_board_y,
+                            attached_field = self,
+                            start_index = boards_n*size_board_x*size_board_y
+                        )
+            self.boards.append(new_board)
+    def __iter__(self, idx):
+        yield self.boards[idx]
 
-WIDTH = 16
-HEIGHT = 8
-NUM_PIXELS = WIDTH*HEIGHT
+    def paint(self, color=COLORS["WHITE"]):
+        for board in self.boards:
+            for row in board:
+                for pixel in row:
+                    pixel.paint(color)
 
-ORDER = neopixel.GRB
+    def show(self):
+        self.neofield.show()
 
-pixels = neopixel.NeoPixel(pixel_pin, NUM_PIXELS, brightness=0.02, auto_write=False, pixel_order=ORDER)
-pixels.fill((0,0,0))
-pixels.show()
+class Board:
+    def __init__(self, size_x, size_y, start_index, attached_field):
+        self.start_index = start_index
+        self.rows = []
+        for downwards in range(size_y):
+            new_row = Row(
+                        size_x,
+                        start_index=start_index + size_x*downwards,
+                        attached_field = self.attached_field
+                    )
+            self.rows.append(new_row)
 
-pixels[0] = (255,255,255)
-pixels[8] = (255,255,255)
-pixels[64] = (255,255,255)
-pixels.show()
+    def __iter__(self, idx):
+        yield self.rows[idx]
+
+    def paint(self, color=COLORS["WHITE"]):
+        for row in self.rows:
+            for row in board:
+                for pixel in row:
+                    pixel.paint(color)
+
+class Row:
+    def __init__(self, size_x, start_index, attached_field):
+        self.start_index = start_index
+        self.pixels = []
+        self.attached_field = attached_field
+        for pixel in range(size_x):
+            new_pixel = Pixel(
+                            index = start_index + pixel,
+                            attached_field = self.attached_field
+                        )
+            self.pixels.append(new_pixel)
+
+    def __iter__(self, idx):
+        yield self.pixels[idx]
+
+    def paint(self, color=COLORS["WHITE"]):
+        for pixel in self.pixels:
+            pixel.paint(color)
+
+class Pixel:
+    def __init__(self, idx, attached_field):
+        self.index = idx
+        self.attached_field = attached_field
+
+    def paint(self, color=COLORS["WHITE"]):
+        self.attached_field[self.idx] = color
+        self.attached_field.show()
+
+my_playfield = Field(boards=2, size_board_x=8, size_board_y=8)
+my_playfield[]
