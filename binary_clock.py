fd71d446922069aa589ff9ce50d955b062c6eff3
mazunki <rolferen@gmail.com>
2019-12-25 01:37:08 +0100

    used paint() func

--- binary_clock.py
+++ binary_clock.py
@@ -15,9 +15,9 @@ def split_two(unit):
 def bin_to_led(num, leds, colour=RED):
     for led in range(len(leds)):
         if num[led]=="1":
-            leds[led] = BLACK
+            leds[led].paint(BLACK)
         else:
-            leds[led] = RED
+            leds[led].paint(RED)
 
 if __name__ == '__main__':
     my_playfield = px.Field(boards=1, size_board_x=8, size_board_y=5)
@@ -26,13 +26,6 @@ if __name__ == '__main__':
     hr_big, hr_smol = my_cols[0], my_cols[1]
     min_big, min_smol = my_cols[3], my_cols[4]
     sec_big, sec_smol = my_cols[6], my_cols[7]
-
-    px.Row.paint(hr_big, RED)
-    px.Row.paint(hr_smol, RED)
-    px.Row.paint(min_big, RED)
-    px.Row.paint(min_smol, RED)
-    px.Row.paint(sec_big, RED)
-    px.Row.paint(sec_smol, RED)
 
     while True:
         hour, minute, second = datetime.now().strftime("%H %M %S").split()
