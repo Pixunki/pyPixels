b38b58da39f3798116bf9d5da497f91a4809f4fc
mazunki <rolferen@gmail.com>
2019-12-25 03:07:02 +0100

    unixepoch

--- unixepochtime.py
+++ unixepochtime.py
@@ -3,7 +3,7 @@ import time
 from datetime import datetime
 
 RED, BLACK, BLUE = px.COLORS["RED"], px.COLORS["BLACK"], px.COLORS["DARKBLUE"]
-PIX_COUNT = 31
+PIX_COUNT = 32
 
 def get_binary_digits(decimal_n, digits=5):
     return format(decimal_n, f"0{digits}b")
@@ -27,5 +27,8 @@ if __name__ == '__main__':
         now_bin = get_binary_digits(int(now), digits=PIX_COUNT)
         # hour
         bin_to_led(now_bin, blob_pixels)
+        for start, end in [(0, 8), (8, 16), (16, 24), (24, 32)]:
+
+            print(str(int(str(now_bin)[start:end], 2)))
 
         time.sleep(1)
