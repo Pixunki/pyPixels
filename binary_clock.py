d0773f1929fb36c57d34c0c11a814dbe114562f7
mazunki <rolferen@gmail.com>
2019-12-25 01:11:03 +0100

    cleaner code

--- binary_clock.py
+++ binary_clock.py
@@ -1,23 +1,17 @@
 import pixel as px
 from pixel import COLORS as CLR
 import time
+from datetime import datetime
 
 RED = CLR["RED"]
 
 if __name__ == '__main__':
     my_playfield = px.Field(boards=1, size_board_x=8, size_board_y=5)
+
     my_cols = my_playfield[0].get_cols()  # we only use the first board
-
-    print(my_cols)
-    print([pixel for pixel in my_cols])
-    hr_big = my_cols[0]
-    hr_smol = my_cols[1]
-
-    min_big = my_cols[3]
-    min_smol = my_cols[4]
-
-    sec_big = my_cols[6]
-    sec_smol = my_cols[7]
+    hr_big, hr_smol = my_cols[0], my_cols[1]
+    min_big, min_smol = my_cols[3], my_cols[4]
+    sec_big, sec_smol = my_cols[6], my_cols[7]
 
     px.Row.paint(hr_big, RED)
     px.Row.paint(hr_smol, RED)
@@ -26,5 +20,7 @@ if __name__ == '__main__':
     px.Row.paint(sec_big, RED)
     px.Row.paint(sec_smol, RED)
 
+    while True:
+        print(datetime.now())
 
     time.sleep(10)
