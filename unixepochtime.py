3d31b88263abe87f1ded9beb5e04972723cc9127
mazunki <rolferen@gmail.com>
2019-12-25 02:44:43 +0100

    unixepoch

--- unixepochtime.py
+++ unixepochtime.py
@@ -21,7 +21,7 @@ def bin_to_led(num, leds, colour=RED, max=0):
             leds[led].paint(BLUE)
 
 if __name__ == '__main__':
-    my_playfield = px.Field(boards=1, size_board_x=8, size_board_y=5)[0]
+    my_playfield = px.Field(boards=1, size_board_x=8, size_board_y=5)
     blob_pixels = my_playfield.get_all()
 
     while True:
