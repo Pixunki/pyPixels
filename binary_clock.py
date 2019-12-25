a6b0c69fe178827c435989f9253e0049a174a3b3
mazunki <rolferen@gmail.com>
2019-12-25 02:09:09 +0100

    max n leds?

--- binary_clock.py
+++ binary_clock.py
@@ -1,11 +1,8 @@
 import pixel as px
-from pixel import COLORS as CLR
 import time
 from datetime import datetime
 
-RED = CLR["RED"]
-BLACK = CLR["BLACK"]
-BLUE = CLR["BLUE"]
+RED, BLACK, BLUE = px.COLORS["RED"], px.COLORS["BLACK"], px.COLORS["BLUE"]
 
 def get_binary_digits(decimal_n, digits=5):
     return format(decimal_n, f"0{digits}b")
@@ -16,14 +13,13 @@ def split_two(unit):
 def bin_to_led(num, leds, colour=RED, max=0):
     for led in range(len(leds)):
         if len(leds)-max > led:
-            continue
+            leds[led].paint(BLACK)
         if num[led]=="1":
             leds[led].paint(RED)
         else:
             leds[led].paint(BLUE)
 
     for led in range(len(leds)-max):
-        leds[led].paint(BLACK)
 
 if __name__ == '__main__':
     my_playfield = px.Field(boards=1, size_board_x=8, size_board_y=5)
@@ -40,11 +36,9 @@ if __name__ == '__main__':
         # hour
         bin_to_led(get_binary_digits(hr[0]), hr_big, max=2)
         bin_to_led(get_binary_digits(hr[1]), hr_smol, max=4)
-
         # minute
         bin_to_led(get_binary_digits(min[0]), min_big, max=3)
         bin_to_led(get_binary_digits(min[1]), min_smol, max=4)
-
         # second
         bin_to_led(get_binary_digits(sec[0]), sec_big, max=3)
         bin_to_led(get_binary_digits(sec[1]), sec_smol, max=4)
