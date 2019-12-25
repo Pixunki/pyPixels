177f827a4f587115d846f2bbf05204746c3d1a51
mazunki <rolferen@gmail.com>
2019-12-25 02:45:06 +0100

    unixepoch

--- unixepochtime.py
+++ unixepochtime.py
@@ -23,6 +23,7 @@ def bin_to_led(num, leds, colour=RED, max=0):
 if __name__ == '__main__':
     my_playfield = px.Field(boards=1, size_board_x=8, size_board_y=5)
     blob_pixels = my_playfield.get_all()
+    print(blob_pixels)
 
     while True:
         now = datetime.now().strftime("%s")
